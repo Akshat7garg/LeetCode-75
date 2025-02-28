16d4ab067455f247cfb7c31a542e42fba3d2eb6b
Akshat7garg <akshat7garg@gmail.com>
2025-03-01 01:52:36 +0530

    Adding 01_Basics folder with fundamental Java programs

---
+++ 01_Basics/HelloWorld.java
@@ -0,0 +1,16 @@
+// This is a simple Java program that prints "Radhe Radhe!" and "Hello World!" to the console.
+// It serves as an introduction to Java syntax and the main method structure.
+
+// Class declaration: The class name should match the filename (HelloWorld.java).
+public class HelloWorld {
+
+    // The main method: This is the entry point of every Java application,.
+    public static void main(String[] args) {
+
+        // Printing "Radhe Radhe!" to the console.
+        System.out.println("Radhe Radhe!");
+
+        // Printing "Hello World!" to the console.
+        System.out.println("Hello World!");
+    }
+}
